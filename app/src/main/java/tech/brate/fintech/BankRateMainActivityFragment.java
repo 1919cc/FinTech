335654f27e715448b73f403b4a3f098b3772fbdc
hui <1919cc@gmail.com>
2017-12-08 17:33:55 +0800

    Initialize

---
+++ app/src/main/java/tech/brate/fintech/BankRateMainActivityFragment.java
@@ -0,0 +1,22 @@
+package tech.brate.fintech;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class BankRateMainActivityFragment extends Fragment {
+
+    public BankRateMainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_bank_rate_main, container, false);
+    }
+}
